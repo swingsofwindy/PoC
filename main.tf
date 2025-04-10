a84083dc6eefeac721e86beebe8f2ec616aaff4b
YenTranSl <tranthikimyen27052004@gmail.com>
2025-04-10 23:57:02 +0700

    chore: add comment to main

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
+# Cau hinh Provider AWS
 provider "aws" {
   profile = "se400" 
   region  = var.region
 }
 
+# Tao cap khoa SSH
 resource "aws_key_pair" "se400" {
   key_name   = "se400_key_pair"
   public_key = file("../.ssh/id_rsa.pub")
 }
 
+# Tao may chu EC2
 resource "aws_instance" "se400" {
   ami           = var.instance_ami
   instance_type = var.instance_type
